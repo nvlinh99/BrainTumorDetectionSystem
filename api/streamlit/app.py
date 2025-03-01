1a645f71f71f3c763cc2e2d1e8cee49a229500d7
Linh Nguyen Vu <linhnv@sendo.vn>
2025-03-01 08:16:24 +0700

    chore: update logic

---
+++ api/streamlit/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import requests
+from PIL import Image
+
+st.set_page_config(
+    page_title="Brain Tumor Detection",
+    page_icon=":brain:",
+    initial_sidebar_state="auto"
+)
+
+FASTAPI_URL = "http://127.0.0.1:8000"
+
+st.title("Brain Tumor Detection")
+
+# Upload file
+file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if file:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+
+    st.subheader("Brain Tumor Detection Results")
+    with st.spinner("Processing..."):
+        response = requests.post(f"{FASTAPI_URL}/predict/brain-tumor", files={"file": file.getvalue()})
+    
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Prediction: {result['predicted_class']}")
+        st.info(f"Confidence: {result['confidence']:.2f}%")
+        if result["predicted_class"] == "No Tumor":
+            st.balloons()
+    else:
+        st.error("Error processing the image!")
