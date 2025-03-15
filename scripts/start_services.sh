15ca0d3b3b69bdc0e4a68640e02ffb699711cdb1
Linh Nguyen Vu <linhnv@sendo.vn>
2025-03-15 09:36:14 +0700

    chore: update worker

--- scripts/start_services.sh
+++ scripts/start_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start FastAPI
-uvicorn api.main:app --host 0.0.0.0 --port 8000 --workers 2 &
+uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit
 streamlit run api/streamlit/app.py --server.port 8501 --server.address 0.0.0.0
